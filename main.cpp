e3cbade143b170dcc39745c78d49bc4f203a4e57
JSchrtke <jschrtke@protonmail.com>
2021-04-12 13:53:48 +0200

    Initial commmit: show hello world message box

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow)
+{
+    MessageBox(NULL, "Hello, World!", "Hello", MB_OK);
+    return 0;
+}
